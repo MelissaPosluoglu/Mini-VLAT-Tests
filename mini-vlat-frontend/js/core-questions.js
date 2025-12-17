5983eb148c4db4115c6ebad19d1c94f0a64f5744
Melissa Posluoglu <st182468@stud.uni-stuttgart.de>
2025-12-17 13:51:00 +0100

    update pics

--- mini-vlat-frontend/js/core-questions.js
+++ mini-vlat-frontend/js/core-questions.js
@@ -2,84 +2,84 @@ const questions = [
    {
         id: "treemap",
         prompt: "eBay is nested in the Software category. True or false?",
-        img: "https://aviz-studies.lisn.upsaclay.fr/readability-baseline/mini-VLAT/treemap.png",
+        img: "../../images/treemap.png",
         answers: ["True", "False","No Answer"],
         correct: "False"
     },
     {
         id: "histogram",
         prompt: "What distance have customers traveled the most?",
-        img: "https://aviz-studies.lisn.upsaclay.fr/readability-baseline/mini-VLAT/histogram.png",
+        img: "../../images/histogram.png",
         answers: ["20–30 km", "50–60 km", "60–70 km", "30–40 km","No Answer"],
         correct: "30–40 km"
     },
     {
         id: "100stacked",
         prompt: "Which country has the lowest proportion of Gold medals?",
-        img: "https://aviz-studies.lisn.upsaclay.fr/readability-baseline/mini-VLAT/100stackedbar.png",
+        img: "../../images/100stacked.png",
         answers: ["U.S.A.", "Great Britain", "Japan", "Australia","No Answer"],
         correct: "Great Britain"
     },
     {
         id: "map",
         prompt: "In 2020, the unemployment rate for Washington (WA) was higher than that of Wisconsin (WI). True or false?",
-        img: "https://aviz-studies.lisn.upsaclay.fr/readability-baseline/mini-VLAT/map.png",
+        img: "../../images/map.png",
         answers: ["True", "False","No Answer"],
         correct: "True"
     },
     {
         id: "pie",
         prompt: "What is the approximate global smartphone market share of Samsung?",
-        img: "https://aviz-studies.lisn.upsaclay.fr/readability-baseline/mini-VLAT/pie.png",
+        img: "../../images/pie.png",
         answers: ["10.9%", "17.6%", "25.3%", "35.2%","No Answer"],
         correct: "17.6%"
     },
     {
         id: "bubble",
         prompt: "Which has the largest number of metro stations?",
-        img: "https://aviz-studies.lisn.upsaclay.fr/readability-baseline/mini-VLAT/bubble.png",
+        img: "../../images/bubble.png",
         answers: ["Beijing", "Shanghai", "London", "Seoul","No Answer"],
         correct: "Shanghai"
     },
     {
         id: "stackedbar",
         prompt: "What is the cost of peanuts in Seoul?",
-        img: "https://aviz-studies.lisn.upsaclay.fr/readability-baseline/mini-VLAT/stackedbar.png",
+        img: "../../images/100stacked.png",
         answers: ["$7.5", "$6.1", "$5.2", "$4.5","No Answer"],
         correct: "$6.1"
     },
     {
         id: "line",
         prompt: "What was the price of a barrel of oil in February 2020?",
-        img: "https://aviz-studies.lisn.upsaclay.fr/readability-baseline/mini-VLAT/line.png",
+        img: "../../images/line.png",
         answers: ["$50.54", "$42.34", "$47.02", "$43.48","No Answer"],
         correct: "$50.54"
     },
     {
         id: "bar",
         prompt: "What is the average internet speed in Japan?",
-        img: "https://aviz-studies.lisn.upsaclay.fr/readability-baseline/mini-VLAT/bar.png",
+        img: "../../images/bar.png",
         answers: ["40.51 Mbps", "16.16 Mbps", "35.25 Mbps", "42.30 Mbps","No Answer"],
         correct: "40.51 Mbps"
     },
     {
         id: "area",
         prompt: "What was the average price of a pound of coffee in October 2019?",
-        img: "https://aviz-studies.lisn.upsaclay.fr/readability-baseline/mini-VLAT/area.png",
+        img: "../../images/area.png",
         answers: ["$0.71", "$0.63", "$0.80", "$0.90","No Answer"],
         correct: "$0.71"
     },
     {
         id: "stackedarea",
         prompt: "What was the ratio of girls named 'Isla' to girls named 'Amelia' in 2012 in the UK?",
-        img: "https://aviz-studies.lisn.upsaclay.fr/readability-baseline/mini-VLAT/stackedarea.png",
+        img: "../../images/stackedarea.png",
         answers: ["1 to 1", "1 to 2", "1 to 3", "1 to 4","No Answer"],
         correct: "1 to 2"
     },
     {
         id: "scatter",
         prompt: "There is a negative relationship between height and weight of the 85 males. True or false?",
-        img: "https://aviz-studies.lisn.upsaclay.fr/readability-baseline/mini-VLAT/scatterplot.png",
+        img: "../../images/scatter.png",
         answers: ["True", "False","No Answer"],
         correct: "False"
     }
